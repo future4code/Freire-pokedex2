51ff5243ee4cf056abd05d3b42093017cbbc4ee7
Maycon Coutinho <mayconcoutinhoart@gmail.com>
2022-07-19 23:38:42 -0300

    Atualizando a pagina detalhes

--- src/hooks/axios.js
+++ src/hooks/axios.js
@@ -4,26 +4,52 @@ import { useEffect, useState } from "react";
 
 // funcionando lista de viagens disponiveis
 export const PokemonSearch = (NomePokemon) => {
-  
-  const [pokemonRecebido, setPokemonRecebido]  = useState([])
+
+  const [pokemonRecebido, setPokemonRecebido] = useState([])
 
   useEffect(() => {
 
     axios
-    .get(
-      `https://pokeapi.co/api/v2/pokemon/pikachu`
-    )
-    .then((response) => {
-      setPokemonRecebido(response)
-      console.log(response)
+      .get(
+        `https://pokeapi.co/api/v2/pokemon/${NomePokemon}`
+      )
+      .then((response) => {
+        setPokemonRecebido(response)
 
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-  },[] )
+  }, [])
 
-    return pokemonRecebido
-    
+  return pokemonRecebido
+
+};
+
+
+
+
+export const LimitadorDeApi = (quantidadePokemon) => {
+
+  const [LimitadorDeApi, setPokemonRecebido] = useState([])
+
+  useEffect(() => {
+
+    axios
+      .get(
+        ` https://pokeapi.co/api/v2/pokemon?limit=${quantidadePokemon}&offset=0`
+      )
+      .then((response) => {
+        setPokemonRecebido(response)
+
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+  }, [])
+
+  return LimitadorDeApi
+
 };
